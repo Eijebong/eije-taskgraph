34f256395e064536af22852b19d08f530f115278
Bastien Orivel <eijebong@bananium.fr>
2024-07-20 20:05:32 +0200

    Fix name

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from distutils.util import convert_path
 from setuptools import find_packages, setup
 
 setup(
-    name="mozilla-taskgraph",
+    name="eije_taskgraph",
     version="0.1.0",
-    description="Eije's taskgrpah tranforms",
+    description="Eije's taskgraph tranforms",
     url="https://github.com/Eijebong/eije-taskgraph",
     packages=find_packages("src"),
     package_dir={"": "src"},
