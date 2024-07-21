9c40315cf6c71e1eb5fce95eb8152f30285c8fb0
Bastien Orivel <eijebong@bananium.fr>
2024-07-21 12:48:53 +0200

    Hijack docker_image so I can change the worker type...

--- src/eije_taskgraph/transforms/podman.py
+++ src/eije_taskgraph/transforms/podman.py
@@ -1,8 +1,15 @@
 from taskgraph.transforms.base import TransformSequence
+from taskgraph.transforms.docker_image import docker_image_schema
+from voluptuous import Schema, Optional, ALLOW_EXTRA
+docker_image_schema.extra = ALLOW_EXTRA
+
 transforms = TransformSequence()
+transforms.add_validate(Schema({
+    Optional("worker-type"): str,
+}, extra = ALLOW_EXTRA))
 
 @transforms.add
-def add_noun(config, tasks):
+def add_container_env(config, tasks):
     for task in tasks:
         task["task"]["payload"]["env"]["container"] = "docker"
 
