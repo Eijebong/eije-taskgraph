8f48417cf763cac5011d25717078fc84e42350d8
Bastien Orivel <eijebong@bananium.fr>
2024-08-20 22:10:52 +0200

    Back to running as root with kaniko

--- src/eije_taskgraph/transforms/rust.py
+++ src/eije_taskgraph/transforms/rust.py
@@ -144,6 +144,7 @@ def publish(config, task):
         "run": {
             "using": "run-task",
             "command": "bash /usr/local/bin/push_image.sh",
+            "run-as-root": True,
         },
         "dependencies": {
             "build": "{}-build".format(config.kind),
