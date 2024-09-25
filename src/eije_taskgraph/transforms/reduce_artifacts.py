8c3b6a53a5875b6e60c572357f1f0bcf47d9978c
Bastien Orivel <eijebong@bananium.fr>
2024-09-25 23:27:20 +0200

    Fix expiration

--- src/eije_taskgraph/transforms/reduce_artifacts.py
+++ src/eije_taskgraph/transforms/reduce_artifacts.py
@@ -1,6 +1,5 @@
 from taskgraph.transforms.base import TransformSequence
 from taskgraph.util.schema import Schema
-from taskgraph.util.time import json_time_from_now
 
 transforms = TransformSequence()
 
@@ -9,5 +8,6 @@ def add_noun(config, tasks):
     for task in tasks:
         for name, artifact in task['task']['payload']['artifacts'].items():
             artifact['expires'] = {'relative-datestamp': '14 days'}
-        task['expires'] = json_time_from_now('14 days')
+        task['task']['expires'] = {'relative-datestamp': '14 days'}
+
         yield task
