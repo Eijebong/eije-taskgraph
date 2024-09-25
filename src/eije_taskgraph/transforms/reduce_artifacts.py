3f011a1b7293fb1ee821b3dd767aa0400b34bf02
Bastien Orivel <eijebong@bananium.fr>
2024-09-25 23:19:21 +0200

    Expire tasks at the same time as artifacts when reducing their lifetime

--- src/eije_taskgraph/transforms/reduce_artifacts.py
+++ src/eije_taskgraph/transforms/reduce_artifacts.py
@@ -1,5 +1,6 @@
 from taskgraph.transforms.base import TransformSequence
 from taskgraph.util.schema import Schema
+from taskgraph.util.time import json_time_from_now
 
 transforms = TransformSequence()
 
@@ -7,6 +8,6 @@ transforms = TransformSequence()
 def add_noun(config, tasks):
     for task in tasks:
         for name, artifact in task['task']['payload']['artifacts'].items():
-            if "logs/" not in name:
-                artifact['expires'] = {'relative-datestamp': '1 day'}
+            artifact['expires'] = {'relative-datestamp': '14 days'}
+        task['expires'] = json_time_from_now('14 days')
         yield task
