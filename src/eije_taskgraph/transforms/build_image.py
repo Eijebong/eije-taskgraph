f41aa73590ddd8a7bf8ac6a39e93b99634f6bb21
Bastien Orivel <eijebong@bananium.fr>
2025-11-01 17:05:20 +0100

    Make the docker images always-target when they need to be

--- src/eije_taskgraph/transforms/build_image.py
+++ src/eije_taskgraph/transforms/build_image.py
@@ -12,6 +12,5 @@ transforms.add_validate(Schema({
 def add_container_env(config, tasks):
     for task in tasks:
         task["task"]["payload"]["env"]["container"] = "docker"
-        task["attributes"]["always_target"] = False
 
         yield task
